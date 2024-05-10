166dc96d08a580ecc29d59e93f96235e95ea28fa
audrey-robak <152941700+audrey-robak@users.noreply.github.com>
2024-05-09 21:31:12 -0500

    done

---
+++ CSS/script.js
@@ -0,0 +1,7 @@
+// Hide the loader when the content is loaded
+window.addEventListener('load', function() {
+    var loader = document.querySelector('.loader');
+    loader.style.display = 'none';
+  });
+  
+  
